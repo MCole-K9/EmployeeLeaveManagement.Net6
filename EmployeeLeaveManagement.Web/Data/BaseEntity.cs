f577568cc166fc09c1bab700ef2d745e6a0bfca6
Matthew Cole <m.b.cole300@gmail.com>
2022-06-05 23:31:41 -0500

    Created Base Class

---
+++ EmployeeLeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeLeaveManagement.Web.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
