46513d3122d327d3fd39d110a0548ef9f4b4da0e
Matthew Cole <m.b.cole300@gmail.com>
2022-06-06 23:54:05 -0500

    Created LeaveType View Model

---
+++ EmployeeLeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeLeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name  = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+
+    }
+}
