6aa68918f67eb3dcf0796e5a2ef98ad5af9521cf
Matthew Cole <m.b.cole300@gmail.com>
2022-06-06 23:54:52 -0500

    Mapping Leave Type Data Model To View Model (Related View changes have already been commited)

---
+++ EmployeeLeaveManagement.Web/Configuration/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EmployeeLeaveManagement.Web.Data;
+using EmployeeLeaveManagement.Web.Models;
+namespace EmployeeLeaveManagement.Web.Configuration
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
--- EmployeeLeaveManagement.Web/Program.cs
+++ EmployeeLeaveManagement.Web/Program.cs
@@ -1,6 +1,8 @@
 using EmployeeLeaveManagement.Web.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using EmployeeLeaveManagement.Web.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
